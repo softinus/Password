de4dd6e3eb65dc4f6ccafa65b8ef524137127631
thks10 <thks10@43e83ec0-4d0e-0510-b797-b29edc70ea32>
2015-03-01 09:53:52 +0000

    -구글 플레이 서비스 소스 폴더 추가. -Manifest meta 정보 추가. -GPGS 연동 완료.

---
+++ cocos2dx_version/proj.android/google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+F:\COCOS_WORK\password\trunk\cocos2dx_version\pw_project\proj.android\google-play-services_lib\bin\AndroidManifest.xml : \
+F:\COCOS_WORK\password\trunk\cocos2dx_version\pw_project\proj.android\google-play-services_lib\AndroidManifest.xml \

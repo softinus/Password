bd4af19f62aed3aa4cf6da8b22b79d377e7040fe
thks10 <thks10@43e83ec0-4d0e-0510-b797-b29edc70ea32>
2015-03-27 09:46:08 +0000

    -added: parse push.

--- trunk/cocos2dx_version/proj.android/src/org/cocos2dx/cpp/MainApp.java
+++ trunk/cocos2dx_version/proj.android/src/org/cocos2dx/cpp/MainApp.java
@@ -1,8 +1,12 @@
 package org.cocos2dx.cpp;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 public class MainApp extends Application {
 
@@ -13,6 +17,16 @@ public class MainApp extends Application {
 		Parse.enableLocalDatastore(this);
 		Parse.initialize(this, "OAWkpBaiKlCiyItu6swfYpjV8Y87DSdeZiqlBOOv", "0vwC4qqhxGtbTTrWcqQyxacafUXG4PYay66N5DrG");
 
+		ParsePush.subscribeInBackground("Password", new SaveCallback() {
+			  @Override
+			  public void done(ParseException e) {
+			    if (e == null) {
+			      Log.d("com.parse.push", "=======successfully subscribed to the broadcast channel.");
+			    } else {
+			      Log.e("com.parse.push", "=======failed to subscribe for push", e);
+			    }
+			  }
+			});
 		
 		super.onCreate();
 	}
